3ca1667662d17c1d7923b99c52a4f5eb77210974
Frenzoid <frenzoid@protonmail.com>
2022-04-11 18:44:43 +0200

    updated prac6 consumer

--- Practicas/Practica 6/Consumer/Consumidor.js
+++ Practicas/Practica 6/Consumer/Consumidor.js
@@ -2,11 +2,11 @@ const mqtt = require('mqtt');
 const trilateration = require('trilateration');
 const client = mqtt.connect('mqtt://oldbox.cloud');
 
-// CAOS
+// Measures
 let distance = [];
 let intensity = [];
 
-distance.push(0);  
+distance.push(0);
 distance.push(25);
 distance.push(50);
 distance.push(75);
@@ -25,13 +25,13 @@ intensity.push(-77);
 intensity.push(-83);
 intensity.push(-85);
 intensity.push(-87);
-intensity.push(-85);
+intensity.push(-89);
 intensity.push(-93);
 intensity.push(-97);
 intensity.push(-99);
 
 
-let arduinos = [Array(), Array(), Array()];
+let arduinos = [[], [], []];
 
 client.on('connect', () => {
     client.subscribe('SE/practicaUA2022/murcia');
@@ -49,37 +49,33 @@ client.on('message', (topic, message) => {
     }
 
     if (arduinos.every(arduino => arduino.length == 31)) {
-        let a = median(arduinos[0]);
-        let b = median(arduinos[1]);
-        let c = median(arduinos[2]);
-
-        a = obtenDistancia(a);
-        b = obtenDistancia(b);
-        c = obtenDistancia(c);
+        let a = obtenDistancia(median(arduinos[0]));
+        let b = obtenDistancia(median(arduinos[1]));
+        let c = obtenDistancia(median(arduinos[2]));
 
         let pos = trilateration(a, b, c);
-        let arduinos = [Array(), Array(), Array()];
+        let arduinos = [[], [], []];
 
-        client.publish('SE/practicaUA2022/arduinocloud', [pos.x, pos.y].toString());
+        client.publish('SE/practicaUA2022/arduinocloud', `${pos.x},${pos.y}`);
     }
 });
 
 // function de trilateracion en dos dimensiones
 function trilateration(a, b, c) {
-    
+
     // Adding three beacons
     trilateration.addBeacon(0, trilateration.vector(2, 4));
     trilateration.addBeacon(1, trilateration.vector(5.5, 13));
     trilateration.addBeacon(2, trilateration.vector(11.5, 2));
-    
+
     // Setting the beacons distances
     trilateration.setDistance(0, a);
     trilateration.setDistance(1, b);
     trilateration.setDistance(2, c);
-    
+
     // Start Calculation
     return trilateration.calculatePosition();
-    
+
 }
 
 // obten la mediana de un array de valores
@@ -95,7 +91,7 @@ function median(values) {
 
 // Obtiene la distancia basada en la intensitynsidad
 function obtenDistancia(ints) {
-    let pos = intensity.findIndex((element) => ints >= element );
+    let pos = intensity.findIndex((element) => ints >= element);
     console.log(pos, distance[pos], distance[pos - 1], intensity[pos], intensity[pos - 1]);
     return distance[pos - 1] + (ints - intensity[pos - 1]) * ((distance[pos] - distance[pos - 1]) / (intensity[pos] - intensity[pos - 1]));
 }
